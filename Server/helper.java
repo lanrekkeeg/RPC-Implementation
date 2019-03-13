687b74d23d6b0b69cc8dc690fddc7882637407bf
Faisal <p156058@nu.edu.pk>
2019-03-13 23:45:37 +0500

    Update helper.java

--- Server/helper.java
+++ Server/helper.java
@@ -1,21 +1,6 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.IntBuffer;
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JOptionPane;
 public class helper {
 
 	
@@ -28,10 +13,12 @@ public class helper {
 		int res = param.get(0) - param.get(1);
 		return res;
 	 }
+	
 	public static int mul(List<Integer>param) {
 		int res = param.get(0) * param.get(1);
 		return res;
 	 }
+	
 	public static int div(List<Integer>param) {
 		int res = param.get(0) / param.get(1);
 		return res;
